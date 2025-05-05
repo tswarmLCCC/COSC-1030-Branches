be066b334ade58bc3d711014f7b57da0da29b942
Trevor Swarm <tswarm@lccc.wy.edu>
2025-05-05 13:26:43 -0600

    Create 5_Make.cpp

---
+++ 5_Make.cpp
@@ -0,0 +1,20 @@
+/*
+Write a program whose inputs are three integers, and whose output is the smallest of the three values.
+
+Ex: If the input is:
+
+7 15 3
+the output is:
+
+3
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+   /* Type your code here. */
+
+   return 0;
+}
